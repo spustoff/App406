3e81458ff8eb81c83cca11ddaf85b3cf947bf8b8
IGOLIVIT <131347905+IGOLIVIT@users.noreply.github.com>
2024-03-13 13:42:14 +0300

    Initial Commit

---
+++ App406/App406App.swift
@@ -0,0 +1,17 @@
+//
+//  App406App.swift
+//  App406
+//
+//  Created by IGOR on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct App406App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
